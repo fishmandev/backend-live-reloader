1a83a7cb2fda9767810b24e87052438d468d9b3f
FishmanDev <fishman.code@gmail.com>
2017-08-14 09:43:00 +0300

    Primitive the 'live reloader' implementation

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
 
-app.get('/reload', (req, res) => {
-  res.send('success')
+const WebSocketServer = require('websocket').server;
+
+
+var server = app.listen(3000, () => {
+  console.log('Live reloader listening on port 3000!')
 })
 
-app.listen(3000, () => {
-  console.log('Live reloader listening on port 3000!')
+var connection;
+var wsServer = new WebSocketServer({
+  httpServer: server
+})
+
+wsServer.on('request', (request) => {
+  connection = request.accept(null, request.origin);
+})
+
+app.get('/reload', (req, res) => {
+  if(connection) {
+    connection.sendUTF('reload')
+  }
+  res.send('success')
 })
